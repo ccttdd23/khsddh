8f7e6de7ad750da480816e2071ecd8076297a1c0
Trung Le <trungle@rta.vn>
2023-04-10 19:26:42 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t0pbQbUSsywupP-c77VxHzDt2Kq0bW1E8xQIpVRqmYY/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JfXIVm_FCBmYZdOyiPFnaVTtWW68dg9QZzTxH1SZRdc/';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
